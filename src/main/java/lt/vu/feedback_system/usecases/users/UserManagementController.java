63a5962d0cccdcdab35e6587e7ccde91da043bf7
Justinas <justinasbukas@gmail.com>
2017-04-21 18:07:24 +0300

    Sutvarkytas admin ir blocked.

--- src/main/java/lt/vu/feedback_system/usecases/users/UserManagementController.java
+++ src/main/java/lt/vu/feedback_system/usecases/users/UserManagementController.java
@@ -1,16 +1,12 @@
 package lt.vu.feedback_system.usecases.users;
-
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import lt.vu.feedback_system.dao.UserDAO;
 import lt.vu.feedback_system.entities.User;
 import org.omnifaces.util.Messages;
-
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-
-
 @Model
 @Slf4j
 public class UserManagementController {
@@ -19,24 +15,4 @@ public class UserManagementController {
     @Inject
     private UserDAO userDAO;
 
-    @Transactional
-    public void setAdmin(boolean current_state) {
-        try {
-            user = userDAO.getUserByEmail(user.getEmail());
-            user.setAdmin(current_state);
-        }
-        catch (javax.persistence.NoResultException ex) {
-            Messages.addGlobalError("Wrong email");
-        }
-    }
-    @Transactional
-    public void setBlock(boolean current_state) {
-        try {
-            user = userDAO.getUserByEmail(user.getEmail());
-            user.setBlocked(current_state);
-        }
-        catch (javax.persistence.NoResultException ex) {
-            Messages.addGlobalError("Wrong email");
-        }
-    }
 }
