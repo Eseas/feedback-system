38059ca46873721736c609d433b6c5794e36addc
Kazimieras Senvaitis <kazimieras.senvaitis@gmail.com>
2017-04-23 19:12:21 +0300

    optimized imports (CTRL + ALT + O)

--- src/main/java/lt/vu/feedback_system/usecases/surveys/RequestSurveysController.java
+++ src/main/java/lt/vu/feedback_system/usecases/surveys/RequestSurveysController.java
@@ -1,21 +1,15 @@
 package lt.vu.feedback_system.usecases.surveys;
 
-import lombok.Getter;
-import lombok.Setter;
 import lt.vu.feedback_system.businesslogic.users.Session;
 import lt.vu.feedback_system.dao.SurveyDAO;
 import lt.vu.feedback_system.entities.Survey;
 import lt.vu.feedback_system.entities.User;
-import lt.vu.feedback_system.entities.questions.*;
 
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 @Named
