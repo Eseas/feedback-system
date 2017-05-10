34ac5c5ffd8312324d0d076c01916e1b3b7b9c48
Kazimieras Senvaitis <kazimieras.senvaitis@gmail.com>
2017-05-10 13:33:41 +0300

    minor refactoring

--- src/main/java/lt/vu/feedback_system/usecases/surveys/RequestSurveysController.java
+++ src/main/java/lt/vu/feedback_system/usecases/surveys/RequestSurveysController.java
@@ -4,18 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 import lt.vu.feedback_system.businesslogic.surveys.SurveyLogic;
 import lt.vu.feedback_system.businesslogic.users.UserContext;
-import lt.vu.feedback_system.dao.SurveyDAO;
 import lt.vu.feedback_system.entities.surveys.Survey;
-import lt.vu.feedback_system.entities.User;
 
-import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.List;
-
-import static java.lang.Boolean.TRUE;
 
 @Named
 @ViewScoped
@@ -27,21 +22,13 @@ public class RequestSurveysController implements Serializable {
     @Setter
     private Boolean adminMode;
 
-    private User user;
-
-
     @Inject
     private SurveyLogic surveyLogic;
-
-    @PostConstruct
-    public void loadUser() {
-        user = userContext.getUser();
-    }
 
     public List<Survey> getSurveys() {
         if (adminMode)
             return surveyLogic.getAllSurveys();
         else
-            return surveyLogic.getUserSurveys(user);
+            return surveyLogic.getUserSurveys(userContext.getUser());
     }
 }
