e99b3b7851922ad3715e4c7f741b4739cec41e71
Marius Sukarevičius <lt.marys@gmail.com>
2017-05-11 08:56:04 +0300

    Need some testing.

---
+++ src/main/java/lt/vu/feedback_system/filters/NoCacheFilter.java
@@ -0,0 +1,35 @@
+package lt.vu.feedback_system.filters;
+
+import lt.vu.feedback_system.businesslogic.users.UserContext;
+
+import javax.faces.application.ResourceHandler;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Expires page.
+ *
+ */
+public class NoCacheFilter implements Filter {
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        if (!req.getRequestURI().startsWith(req.getContextPath() + ResourceHandler.RESOURCE_IDENTIFIER)) { // Skip JSF resources (CSS/JS/Images/etc)
+            res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+            res.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+            res.setDateHeader("Expires", 0); // Proxies.
+        }
+
+        chain.doFilter(req, res);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+}
