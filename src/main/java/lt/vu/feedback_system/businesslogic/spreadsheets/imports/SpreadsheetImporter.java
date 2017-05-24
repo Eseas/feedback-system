351342154d210e31fa0b57a80311db4a5fda3fe0
Lukas <mikelionislukas@yahoo.com>
2017-05-24 22:46:51 +0300

    implement file import example workbook download

--- src/main/java/lt/vu/feedback_system/businesslogic/spreadsheets/imports/SpreadsheetImporter.java
+++ src/main/java/lt/vu/feedback_system/businesslogic/spreadsheets/imports/SpreadsheetImporter.java
@@ -2,6 +2,7 @@ package lt.vu.feedback_system.businesslogic.spreadsheets.imports;
 
 import lt.vu.feedback_system.businesslogic.spreadsheets.SpreadsheetException;
 import lt.vu.feedback_system.entities.surveys.Survey;
+import org.primefaces.model.StreamedContent;
 import org.primefaces.model.UploadedFile;
 
 import java.io.IOException;
@@ -9,5 +10,7 @@ import java.io.IOException;
 public interface SpreadsheetImporter {
 
     void importSurvey(final Survey survey, UploadedFile file) throws SpreadsheetException, IOException;
+
+    StreamedContent getImportExample();
 
 }
